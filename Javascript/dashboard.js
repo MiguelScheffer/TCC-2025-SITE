e2daec3121b2e309e226d33f1c09c7526b3e4bcf
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2025-05-16 21:54:29 -0300

    Delete Javascript directory

--- Javascript/dashboard.js
+++
@@ -1,7 +0,0 @@
-const mensagens = ["bora exercitar?", "tá gordo, bora se mecher?", "caga mole"];
-
-const mensagemAleatoria =
-  mensagens[Math.floor(Math.random() * mensagens.length)];
-
-document.querySelector(".headerMensagemDiaria").textContent +=
-  mensagemAleatoria;
