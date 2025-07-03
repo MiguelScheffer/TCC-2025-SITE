51106c47998417155840367a4d171539b665cfb3
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2025-07-03 13:21:24 -0300

    Delete Javascript directory

--- Javascript/adicionarExer.js
+++
@@ -1,8 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-        const treinoId = params.get("id_treino");
-
-        if (treinoId) {
-            document.getElementById("id_treino_hidden").value = treinoId;
-        } else {
-            alert("Erro: treino_id não encontrado na URL.");
-        }
--- Javascript/scrollReveal.js
+++
@@ -1,5 +0,0 @@
-ScrollReveal().reveal('.headline',{ delay: 1000 });
-ScrollReveal().reveal('.tagline', { delay: 500 });
-ScrollReveal().reveal('.punchline', { delay: 2000 });
-
-
